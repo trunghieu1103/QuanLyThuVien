62cfb68f3ead14780f74cb1a3e7b3a90d7dd2481
TrungHieu <trunghieudoan088@gmail.com>
2025-08-22 10:02:46 +0700

    Thêm control vào formQuanLySach

--- QuanLyThuVien/FormTrangChu.cs
+++ QuanLyThuVien/FormTrangChu.cs
@@ -22,15 +22,15 @@ namespace QuanLyThuVien
         private void toolStripMenuItem2_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Bạn có chắc muốn thoát ứng dụng?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if(result == DialogResult.Yes)
+            if (result == DialogResult.Yes)
             {
                 FormDangNhap formDangNhap = new FormDangNhap();
                 formDangNhap.Show();
-               
+
                 this.Close(); // Đóng form Trang Chủ sau khi đăng xuất
-                
+
             }
-            
+
         }
 
         private void báoCáoSốLượngSáchToolStripMenuItem_Click(object sender, EventArgs e)
@@ -56,6 +56,13 @@ namespace QuanLyThuVien
         private void toolStripStatusLabel1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void quảnLýSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQuanLySach formQuanLySach = new FormQuanLySach();
+            formQuanLySach.Show();
+            this.Close();
         }
     }
 }
