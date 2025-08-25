a9d98c8b18bd84ccd0f738eff4e94f785a963139
TrungHieu <trunghieudoan088@gmail.com>
2025-08-25 20:55:18 +0700

    Thêm chức năng thoát

---
+++ QuanLyThuVien/FormCT_PhieuMuon.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace QuanLyThuVien
+{
+    partial class FormCT_PhieuMuon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel3 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel4 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel5 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel6 = new Krypton.Toolkit.KryptonLabel();
+            kryptonLabel7 = new Krypton.Toolkit.KryptonLabel();
+            kryptonComboBox1 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonComboBox2 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonComboBox3 = new Krypton.Toolkit.KryptonComboBox();
+            kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            kryptonDateTimePicker1 = new Krypton.Toolkit.KryptonDateTimePicker();
+            kryptonRichTextBox1 = new Krypton.Toolkit.KryptonRichTextBox();
+            kryptonDataGridView1 = new Krypton.Toolkit.KryptonDataGridView();
+            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            kryptonButton3 = new Krypton.Toolkit.KryptonButton();
+            kryptonButton4 = new Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(693, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.Logo_Đại_học_Bách_Khoa_Hà_Nội_svg;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(827, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(98, 147);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // kryptonLabel1
+            // 
+            kryptonLabel1.Location = new Point(210, 12);
+            kryptonLabel1.Name = "kryptonLabel1";
+            kryptonLabel1.Size = new Size(402, 49);
+            kryptonLabel1.StateCommon.ShortText.Color1 = Color.MediumSlateBlue;
+            kryptonLabel1.StateCommon.ShortText.Color2 = Color.FromArgb(255, 192, 192);
+            kryptonLabel1.StateCommon.ShortText.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Linear;
+            kryptonLabel1.StateCommon.ShortText.Font = new Font("Georgia", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLabel1.TabIndex = 2;
+            kryptonLabel1.Values.Text = "Chi tiết phiếu mượn";
+            // 
+            // kryptonLabel2
+            // 
+            kryptonLabel2.Location = new Point(190, 78);
+            kryptonLabel2.Name = "kryptonLabel2";
+            kryptonLabel2.Size = new Size(151, 24);
+            kryptonLabel2.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonLabel2.TabIndex = 3;
+            kryptonLabel2.Values.Text = "Mã phiếu mượn";
+            // 
+            // kryptonLabel3
+            // 
+            kryptonLabel3.Location = new Point(190, 122);
+            kryptonLabel3.Name = "kryptonLabel3";
+            kryptonLabel3.Size = new Size(86, 24);
+            kryptonLabel3.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonLabel3.TabIndex = 4;
+            kryptonLabel3.Values.Text = "Mã sách";
+            // 
+            // kryptonLabel4
+            // 
+            kryptonLabel4.Location = new Point(190, 165);
+            kryptonLabel4.Name = "kryptonLabel4";
+            kryptonLabel4.Size = new Size(107, 24);
+            kryptonLabel4.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonLabel4.TabIndex = 5;
+            kryptonLabel4.Values.Text = "Mã thủ thư";
+            // 
+            // kryptonLabel5
+            // 
+            kryptonLabel5.Location = new Point(191, 253);
+            kryptonLabel5.Name = "kryptonLabel5";
+            kryptonLabel5.Size = new Size(88, 24);
+            kryptonLabel5.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonLabel5.TabIndex = 6;
+            kryptonLabel5.Values.Text = "Ngày trả";
+            // 
+            // kryptonLabel6
+            // 
+            kryptonLabel6.Location = new Point(190, 208);
+            kryptonLabel6.Name = "kryptonLabel6";
+            kryptonLabel6.Size = new Size(132, 24);
+            kryptonLabel6.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonLabel6.TabIndex = 7;
+            kryptonLabel6.Values.Text = "Số lượng sách";
+            // 
+            // kryptonLabel7
+            // 
+            kryptonLabel7.Location = new Point(534, 90);
+            kryptonLabel7.Name = "kryptonLabel7";
+            kryptonLabel7.Size = new Size(82, 24);
+            kryptonLabel7.StateCommon.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLabel7.TabIndex = 8;
+            kryptonLabel7.Values.Text = "Ghi chú";
+            // 
+            // kryptonComboBox1
+            // 
+            kryptonComboBox1.DropDownWidth = 119;
+            kryptonComboBox1.Location = new Point(347, 78);
+            kryptonComboBox1.Name = "kryptonComboBox1";
+            kryptonComboBox1.Size = new Size(125, 26);
+            kryptonComboBox1.StateCommon.ComboBox.Border.Rounding = 10F;
+            kryptonComboBox1.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox1.TabIndex = 9;
+            kryptonComboBox1.Text = "Chọn mã phiếu";
+            // 
+            // kryptonComboBox2
+            // 
+            kryptonComboBox2.DropDownWidth = 119;
+            kryptonComboBox2.Location = new Point(347, 122);
+            kryptonComboBox2.Name = "kryptonComboBox2";
+            kryptonComboBox2.Size = new Size(125, 26);
+            kryptonComboBox2.StateCommon.ComboBox.Border.Rounding = 10F;
+            kryptonComboBox2.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox2.TabIndex = 10;
+            kryptonComboBox2.Text = "Chọn mã sách";
+            // 
+            // kryptonComboBox3
+            // 
+            kryptonComboBox3.DropDownWidth = 119;
+            kryptonComboBox3.Location = new Point(347, 165);
+            kryptonComboBox3.Name = "kryptonComboBox3";
+            kryptonComboBox3.Size = new Size(125, 26);
+            kryptonComboBox3.StateCommon.ComboBox.Border.Rounding = 10F;
+            kryptonComboBox3.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonComboBox3.TabIndex = 11;
+            kryptonComboBox3.Text = "Chọn mã thủ thư";
+            // 
+            // kryptonTextBox1
+            // 
+            kryptonTextBox1.Location = new Point(347, 208);
+            kryptonTextBox1.Name = "kryptonTextBox1";
+            kryptonTextBox1.Size = new Size(125, 33);
+            kryptonTextBox1.StateCommon.Border.Rounding = 10F;
+            kryptonTextBox1.TabIndex = 12;
+            // 
+            // kryptonDateTimePicker1
+            // 
+            kryptonDateTimePicker1.Location = new Point(347, 253);
+            kryptonDateTimePicker1.Name = "kryptonDateTimePicker1";
+            kryptonDateTimePicker1.Size = new Size(125, 31);
+            kryptonDateTimePicker1.StateCommon.Border.Rounding = 10F;
+            kryptonDateTimePicker1.TabIndex = 13;
+            // 
+            // kryptonRichTextBox1
+            // 
+            kryptonRichTextBox1.Location = new Point(534, 120);
+            kryptonRichTextBox1.Name = "kryptonRichTextBox1";
+            kryptonRichTextBox1.Size = new Size(125, 110);
+            kryptonRichTextBox1.StateCommon.Border.Rounding = 10F;
+            kryptonRichTextBox1.TabIndex = 14;
+            kryptonRichTextBox1.Text = "";
+            // 
+            // kryptonDataGridView1
+            // 
+            kryptonDataGridView1.BorderStyle = BorderStyle.None;
+            kryptonDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            kryptonDataGridView1.Location = new Point(-2, 345);
+            kryptonDataGridView1.Name = "kryptonDataGridView1";
+            kryptonDataGridView1.RowHeadersWidth = 51;
+            kryptonDataGridView1.Size = new Size(927, 188);
+            kryptonDataGridView1.TabIndex = 15;
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.Location = new Point(163, 291);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(112, 31);
+            kryptonButton1.StateCommon.Back.Color1 = Color.MediumSeaGreen;
+            kryptonButton1.StateCommon.Back.Color2 = Color.FromArgb(255, 255, 128);
+            kryptonButton1.StateCommon.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Linear;
+            kryptonButton1.StateCommon.Border.Rounding = 10F;
+            kryptonButton1.StateCommon.Content.ShortText.Color1 = Color.White;
+            kryptonButton1.StateCommon.Content.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonButton1.TabIndex = 16;
+            kryptonButton1.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton1.Values.Text = "Thêm ";
+            // 
+            // kryptonButton2
+            // 
+            kryptonButton2.Location = new Point(314, 291);
+            kryptonButton2.Name = "kryptonButton2";
+            kryptonButton2.Size = new Size(112, 31);
+            kryptonButton2.StateCommon.Back.Color1 = Color.Goldenrod;
+            kryptonButton2.StateCommon.Back.Color2 = Color.FromArgb(255, 255, 192);
+            kryptonButton2.StateCommon.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Linear;
+            kryptonButton2.StateCommon.Border.Rounding = 10F;
+            kryptonButton2.StateCommon.Content.ShortText.Color1 = Color.White;
+            kryptonButton2.StateCommon.Content.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonButton2.TabIndex = 17;
+            kryptonButton2.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton2.Values.Text = "Sửa";
+            // 
+            // kryptonButton3
+            // 
+            kryptonButton3.Location = new Point(450, 291);
+            kryptonButton3.Name = "kryptonButton3";
+            kryptonButton3.Size = new Size(112, 31);
+            kryptonButton3.StateCommon.Back.Color1 = Color.IndianRed;
+            kryptonButton3.StateCommon.Back.Color2 = Color.FromArgb(255, 192, 192);
+            kryptonButton3.StateCommon.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Linear;
+            kryptonButton3.StateCommon.Border.Rounding = 10F;
+            kryptonButton3.StateCommon.Content.ShortText.Color1 = Color.White;
+            kryptonButton3.StateCommon.Content.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonButton3.TabIndex = 18;
+            kryptonButton3.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton3.Values.Text = "Xóa";
+            // 
+            // kryptonButton4
+            // 
+            kryptonButton4.Location = new Point(593, 291);
+            kryptonButton4.Name = "kryptonButton4";
+            kryptonButton4.Size = new Size(112, 31);
+            kryptonButton4.StateCommon.Back.Color1 = Color.SlateGray;
+            kryptonButton4.StateCommon.Back.Color2 = Color.FromArgb(224, 224, 224);
+            kryptonButton4.StateCommon.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.Linear;
+            kryptonButton4.StateCommon.Border.Rounding = 10F;
+            kryptonButton4.StateCommon.Content.ShortText.Color1 = Color.White;
+            kryptonButton4.StateCommon.Content.ShortText.Font = new Font("Georgia", 10.2F, FontStyle.Bold);
+            kryptonButton4.TabIndex = 19;
+            kryptonButton4.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton4.Values.Text = "Thoát";
+            kryptonButton4.Click += kryptonButton4_Click;
+            // 
+            // FormCT_PhieuMuon
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(921, 450);
+            Controls.Add(kryptonButton4);
+            Controls.Add(kryptonButton3);
+            Controls.Add(kryptonButton2);
+            Controls.Add(kryptonButton1);
+            Controls.Add(kryptonDataGridView1);
+            Controls.Add(kryptonRichTextBox1);
+            Controls.Add(kryptonDateTimePicker1);
+            Controls.Add(kryptonTextBox1);
+            Controls.Add(kryptonComboBox3);
+            Controls.Add(kryptonComboBox2);
+            Controls.Add(kryptonComboBox1);
+            Controls.Add(kryptonLabel7);
+            Controls.Add(kryptonLabel6);
+            Controls.Add(kryptonLabel5);
+            Controls.Add(kryptonLabel4);
+            Controls.Add(kryptonLabel3);
+            Controls.Add(kryptonLabel2);
+            Controls.Add(kryptonLabel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "FormCT_PhieuMuon";
+            Text = "Chi tiết phiếu mượn";
+            Load += FormCT_PhieuMuon_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonComboBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel4;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel5;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel6;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel7;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox2;
+        private Krypton.Toolkit.KryptonComboBox kryptonComboBox3;
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Krypton.Toolkit.KryptonDateTimePicker kryptonDateTimePicker1;
+        private Krypton.Toolkit.KryptonRichTextBox kryptonRichTextBox1;
+        private Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonButton kryptonButton3;
+        private Krypton.Toolkit.KryptonButton kryptonButton4;
+    }
+}
---
+++ QuanLyThuVien/FormCT_PhieuMuon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien
+{
+    public partial class FormCT_PhieuMuon : Form
+    {
+        public FormCT_PhieuMuon(string username)
+        {
+            InitializeComponent();
+            label1.Text = username;
+            label1.Hide();
+        }
+
+        private void FormCT_PhieuMuon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonButton4_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn thoát! ", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                FormTrangChu formTrangChu = new FormTrangChu(label1.Text);
+                formTrangChu.Show();
+                this.Close();
+            }
+        }
+    }
+}
